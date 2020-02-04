1fd98b63bbeb31a2bde812ca716fed88e77638cb
Maxence <maxence.delacroix@epitech.eu>
2020-02-04 23:27:12 +0100

    adding blinking effect + new animation

--- TheBard/Assets/Scripts/Entities/Entity.cs
+++ TheBard/Assets/Scripts/Entities/Entity.cs
@@ -40,16 +40,40 @@ public class Entity : MonoBehaviour, IEntity
     {
         if (damage > 0)
             Health -= damage;
+        GetComponent<Animator>().Play("Knight_Attack");
     }
 
     protected virtual void OnHealthChange(int value)
-    { /*Update lifebar, activate red or green blink, play sound...*/}
+    {
+        /*Update lifebar, activate red or green blink, play sound...*/
+        Debug.LogError(GetComponent<SpriteRenderer>().color);
+        StartCoroutine(FlashObject(GetComponent<SpriteRenderer>(), GetComponent<SpriteRenderer>().color, Color.red, 0.5f, 0.1f));
+    }
 
     protected virtual void Die()
     {
         Debug.Log(Type.ToString() + Id + " -- DEAD");
         Destroy(gameObject);
     }
+
+    IEnumerator FlashObject(SpriteRenderer toFlash, Color originalColor, Color flashColor, float flashTime, float flashSpeed)
+    {
+        float flashingFor = 0;
+        var newColor = flashColor;
+        while (flashingFor < flashTime)
+        {
+            toFlash.color = newColor;
+            flashingFor += Time.deltaTime;
+            yield return new WaitForSeconds(flashSpeed);
+            flashingFor += flashSpeed;
+            if (newColor == flashColor)
+                newColor = originalColor;
+            else
+                newColor = flashColor;
+        }
+        toFlash.color = originalColor;
+    }
+
 
     private void OnDestroy()
     {
