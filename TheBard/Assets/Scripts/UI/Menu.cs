dccff567ad29aab138da3f6d70bc9ad80693db3f
Maxence <maxence.delacroix@epitech.eu>
2020-02-05 22:47:08 +0100

    adding basic menu

---
+++ TheBard/Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void Quit()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
